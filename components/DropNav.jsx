3ea70f9eaa684d030425f327686011bed01463b5
DivineDomokuma10 <divinedomokuma@gmail.com>
2023-10-03 21:46:36 +0100

    still on nav functionality

--- components/DropNav.jsx
+++ components/DropNav.jsx
@@ -6,13 +6,13 @@ export const DropNav = () => {
   const { navItems, handleNavState } = useContext(Store);
 
   return (
-    <nav className="w-3/4 flex flex-col items-center space-y-5 fixed top-20 bg-teal-50 rounded-md p-4 sm:hidden">
+    <nav className="w-3/4 flex flex-col items-center space-y-5 fixed top-20 bg-opacity-70 bg-teal-50 rounded-md p-4 sm:hidden">
       {navItems.map((navItem) => (
         <a
           key={navItem.id}
           href={navItem.path}
           onClick={() => handleNavState(navItem.id)}
-          className={`font-semibold text-2xl tracking-[.3rem] ${
+          className={`font-semibold text-2xl tracking-[.2rem] ${
             navItem.isActive ? "text-red-500" : ""
           } hover:text-red-500 hover:scale-105`}
         >
